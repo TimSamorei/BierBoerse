4ca4edc8805c361bc195fe3e697484f0afd4cafa
mfbehrens99 <mfbehrens99@gmail.com>
2022-05-17 19:21:53 +0200

    Update TestClient.py

--- Backend/TestClient.py
+++ Backend/TestClient.py
@@ -1,11 +1,16 @@
+import locale
+
 import grpc
 import Bierboerse_pb2_grpc
 import Bierboerse_pb2
 
+locale.setlocale(locale.LC_ALL, "german")
+
 def printPricelist(datapoint: Bierboerse_pb2.Datapoint):
     print("Prices at", datapoint.timestamp)
+    print("ID\tNAME\tPRICE\tSOLD")
     for bev in datapoint.beverages:
-        print(bev.id, "\t", bev.name, "\t", bev.currentPrice / 100, "€")
+        print(bev.id, "\t", bev.name, "\t", locale.currency(bev.currentPrice / 100), "\t", bev.sold)
     print("=========================")
 
 
@@ -17,9 +22,9 @@ printPricelist(datapoint)
 
 while(True):
 
-    buyId = int(input("Buy: "))
+    buyIndex = int(input("Buy: "))
 
-    req = Bierboerse_pb2.BuyRequest(buyId=buyId)
+    req = Bierboerse_pb2.BuyRequest(buyIndex=buyIndex)
     datapoint = stub.buyBeverage(req)
 
     printPricelist(datapoint)
